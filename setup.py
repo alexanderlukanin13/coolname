fce210d266fec9e5341ebc76961cf78fa33036c1
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2016-09-28 21:21:49 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
     zip_safe=True,
     keywords='coolname',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
