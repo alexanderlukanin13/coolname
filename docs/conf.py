73011404dde2d2a114d070f5bfdd77a1557dcbcf
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2016-09-28 21:09:03 +0500

    docs version -> 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ author = 'Alexander Lukanin'
 # built documents.
 #
 # The short X.Y version.
-version = '0.1'
+version = '0.2'
 # The full version, including alpha/beta/rc tags.
 release = '0.2.0'
 
