c7cfe983f38ea17b763f41eb2e84d0c609c12f73
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2018-02-17 16:08:21 +0500

    Bump version: 1.0.2 → 1.0.3

--- coolname/__init__.py
+++ coolname/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 # Hint: set COOLNAME_DATA_DIR and/or COOLNAME_DATA_MODULE
 # before `import coolname` to change the default generator.
--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ test_requirements = [
 
 setup(
     name='coolname',
-    version='1.0.2',
+    version='1.0.3',
     description="Random name and slug generator",
     long_description=readme + '\n\n' + history,
     author="Alexander Lukanin",
