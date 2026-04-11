01a581a771ccd2b8bbc05283f4230fcfb96db301
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2026-04-11 22:56:06 +0600

    export __all__

--- src/coolname/__init__.py
+++ src/coolname/__init__.py
@@ -3,6 +3,12 @@ from ._version import __version__, __version_tuple__
 # Hint: set COOLNAME_DATA_DIR and/or COOLNAME_DATA_MODULE
 # before `import coolname` to change the default generator.
 
-from .exceptions import InitializationError
+from .exceptions import InitializationError, ConfigurationError
 from .impl import generate, generate_slug, get_combinations_count,\
     RandomGenerator, replace_random
+
+__all__ = [
+    'generate', 'generate_slug', 'get_combinations_count', 'replace_random',
+    'RandomGenerator',
+    'InitializationError', 'ConfigurationError',
+]
